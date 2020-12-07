2dc1da3765a01040c6fcdb2024b1033bd5ac4395
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-12-07 11:36:09 +0100

    3 pages link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
@@ -10,6 +10,22 @@ function App() {
       <div className="row">
         <div className="col-md-12">
           <h1>Fakestagram</h1>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-12">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
         </div>
       </div>
 
