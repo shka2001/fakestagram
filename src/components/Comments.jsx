a78837215fc9dfbd5b2caae58e03602108bcaf41
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-12-08 12:07:38 +0100

    Commet Lägg till det i Comment List if add

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,46 +1,31 @@
 import React, {useRef, useState} from 'react'
 
 export default function Comments() {
-    const inputRef = useRef()
-    const [commentList, setCommentList] = useState(["hej", "hå"])
-    /*const commentList = [
-        "hej",
-        "tjsna",
-        "koolt"
-    ]*/
+  const inputRef = useRef()
+  const [commentList, setCommentList] = useState(["hej", "hå"])
 
-    function handleOmClick() {
-        //Hämta ut det användaren har skrivit i input fältet
-        const value = inputRef.current.value
-        //console.log(inputRef.current.value)
-        //document.getElementById("commentInput").value
-        
-        //Lägg till det i Comment List
-        /*
-        commentList.push(value)×
-        console.log(commentList)×
-        */
-        setCommentList([...commentList, value])
-        /*
-        上と同じこと
-        const input = inputRef.current 
-        input.value = ""
-        */
+  function handleOnClick() {
+    // Hämta ut det användaren har skrivit i input fältet
+    const input = inputRef.current
+    // Lägg till det i Comment List
+    if(input.value !== "") {
+      setCommentList( [input.value, ...commentList] )
     }
+    input.value = ""
+  }
 
-    return (
-        <div>
-            {/*Comment List*/}
-            {commentList.map((comment, index) =>{
-                return <p key={index}>{comment}</p>
-            })}
-            <input ref={inputRef} type="text" />
-            <button 
-            onClick={handleOmClick} 
-            className="btn btn-primary"
-            >
-                Send
-            </button>
-        </div>
-    )
+  return (
+    <div>
+      {commentList.map((comment, index) => {
+        return <p key={index}>{comment}</p>
+      })}
+      <input ref={inputRef} type="text" />
+      <button 
+        onClick={handleOnClick} 
+        className="btn btn-primary"
+      >
+        Send
+      </button>
+    </div>
+  )
 }
