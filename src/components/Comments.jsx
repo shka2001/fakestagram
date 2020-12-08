ecc139a2c693f536afb848936d31537b702b6b72
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-12-08 11:32:42 +0100

    Comments Lägg till det i Comment List

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,17 +2,29 @@ import React, {useRef} from 'react'
 
 export default function Comments() {
     const inputRef = useRef()
-
+    const commentList = [
+        "hej",
+        "tjsna",
+        "koolt"
+    ]
 
     function handleOmClick() {
         //Hämta ut det användaren har skrivit i input fältet
-        document.getElementById("commentInput").value
+        const value = inputRef.current.value
+        //console.log(inputRef.current.value)
+        //document.getElementById("commentInput").value
         //Lägg till det i Comment List
+        commentList.push(value)
+        console.log(commentList)
     }
+
     return (
         <div>
-            Comment List
-            <input id="commentInput" type="text" />
+            {/*Comment List*/}
+            {commentList.map((comment, index) =>{
+                return <p key={index}>{comment}</p>
+            })}
+            <input ref={inputRef} type="text" />
             <button 
             onClick={handleOmClick} 
             className="btn btn-primary"
